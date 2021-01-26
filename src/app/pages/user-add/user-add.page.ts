d5527b5604a360d76e8f83e96d2104ab615be825
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-01-25 21:43:26 -0300

    Aula02

---
+++ src/app/pages/user-add/user-add.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-add',
+  templateUrl: './user-add.page.html',
+  styleUrls: ['./user-add.page.scss'],
+})
+export class UserAddPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'userAdd',
+        loadChildren: () => import('../pages/user-add/user-add.module').then( m => m.UserAddPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
